48a1a2b6343d726feee477bd4d5cb2b3a3b6927c
James Ferguson <ncjamesferguson@gmail.com>
2017-12-21 23:48:24 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,11 +2,14 @@ require_relative 'backer.rb'
 
 class Project
 
+  @@projects = []
+
   attr_accessor :title, :backers
 
   def initialize(title)
     self.title = title
     @backers = []
+    @@projects << self
   end
 
     def add_backer(backer)
